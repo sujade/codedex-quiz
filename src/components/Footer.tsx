429fc4184de227e90e33350e1fb104454c52c0fb
sujade <dnmzsu@gmail.com>
2024-04-09 17:19:57 +0200

    corrected footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <Container id={"footer"}
                    className={"footer d-flex flex-column align-items-center justify-content-center text-center mt-5 border border-3 border-warning rounded p-3"}>
             <p>This personality quiz is made for <a
-                href={"https://www.codedex.io/community/monthly-challenge/submission/JDOP2gqJh12IalbyRy6Y"}> Codédex
+                href={"https://www.codedex.io/community/monthly-challenge/submission/Vsf2seRgMVItdQ8iilXE"}> Codédex
                 Monthly Challenge!</a></p>
             <p>Made with lots of <FaHeart/> by <a href={"https://github.com/sujade"}>Su Jade</a></p>
             <a href={"/sources"}>Sources</a>
