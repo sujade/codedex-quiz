355afd5432977b502ea9394bea52ff3fe8b53117
sujade <dnmzsu@gmail.com>
2024-04-14 09:51:27 +0200

    add return button to sources page

--- src/app/sources/page.tsx
+++ src/app/sources/page.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import GoBackButton from "@/components/ResultInstance/GoBackButton";
 
 const Page = () => {
     return (
         <Container className={"d-flex justify-content-center center-content mt-5"}>
-            <ul>
-                <li>Spinning Pixel Coin by <a href={"https://opengameart.org/users/irmirx"}>irmirx</a></li>
-                <li>Sound Effect from <a
-                    href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6104">Pixabay</a>
-                </li>
-                <li>Background art by <a href={"https://ko-fi.com/s/5b6d5672fa"}>Tofu</a></li>
-                <li>Phantasm by <a href={"https://soundcloud.com/purelygrey"}>Purely Grey</a></li>
-                <li>Music promoted by <a href={"https://www.free-stock-music.com"}>free-stock-music</a></li>
-
-            </ul>
+            <Row>
+                <Col>
+                <ul>
+                    <li>Spinning Pixel Coin by <a href={"https://opengameart.org/users/irm5irx"}>irmirx</a></li>
+                    <li>Sound Effect from <a
+                        href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6104">Pixabay</a>
+                    </li>
+                    <li>Background art by <a href={"https://ko-fi.com/s/5b6d5672fa"}>Tofu</a></li>
+                    <li>Phantasm by <a href={"https://soundcloud.com/purelygrey"}>Purely Grey</a></li>
+                    <li>Music promoted by <a href={"https://www.free-stock-music.com"}>free-stock-music</a></li>
+                </ul>
+                    <div className={"d-flex justify-content-center"}>
+                <GoBackButton/>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 };
